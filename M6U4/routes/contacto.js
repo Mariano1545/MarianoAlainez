9509d75dcd9187898ec0f387ce41819ef374083e
mariano1545 <mariano_alaines@hotmail.com>
2024-03-26 00:44:21 -0300

    Entrega Final

---
+++ M6U4/routes/contacto.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var contactoModel = require('../models/contactoModel');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('contacto',{
+    isContacto: true
+  });
+});
+
+router.post('/', async (req, res, next) => {
+
+  console.log(req.body)
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var telefono = req.body.telefono;
+  var ubicacion = req.body.ubicacion;
+  var comentario = req.body.comentario;
+
+  var obj = {
+    to: 'mariano_alaines@hotmail.com',
+    subject: 'contacto desde la web',
+    html: nombre + "se contacto atraves y quiere mas info a este correo: " + email + ". <br> Ademas, hizo el siguiente comentario: " + comentario + ". <br> Su tel es: " + telefono + ".<br> de la ciudad de: " + ubicacion
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+  var contacto = await contactoModel.insertContacto(req.body)
+
+
+  res.render('contacto', {
+    isContacto: true,
+    message: 'Mensaje enviado correctamente'
+  });
+
+});
+
+module.exports = router;
